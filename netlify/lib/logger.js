9bbc764d5fa4957c7c1150442d3ad9ac0025feae
Alex Vandiver <alex@chmrr.net>
2021-02-09 20:02:57 -0800

    Consistent logging even with stackdriver off

--- netlify/lib/logger.js
+++ netlify/lib/logger.js
@@ -2,40 +2,42 @@ const bunyan = require("bunyan");
 const { LoggingBunyan } = require("@google-cloud/logging-bunyan");
 
 initLogging = (event) => {
+  var streams = [
+    // Log to the console at 'info' and above
+    { stream: process.stdout, level: "info" },
+  ];
+  var end_request = () => true;
+
   if (!process.env.GCP_CLIENT_EMAIL || !process.env.GCP_PRIVATE_KEY) {
     console.log("No Stackdriver logging configured, skipping");
-    return [console, async (resp) => resp];
-  }
-  const options = {
-    projectId: "cavaccineinventory",
-    logName: "netlify",
-    credentials: {
-      client_email: process.env.GCP_CLIENT_EMAIL,
-      private_key: process.env.GCP_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    },
-  };
+  } else {
+    const options = {
+      projectId: "cavaccineinventory",
+      logName: "netlify",
+      credentials: {
+        client_email: process.env.GCP_CLIENT_EMAIL,
+        private_key: process.env.GCP_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      },
+    };
 
-  const stackdriver = new LoggingBunyan(options);
-  const stackdriverStream = stackdriver.stream("info");
+    const stackdriver = new LoggingBunyan(options);
+    const stackdriverStream = stackdriver.stream("info");
+    streams.append(stackdriverStream);
+
+    end_request = () => {
+      if (stackdriverStream.writableFinished) return;
+      return new Promise((res, rej) => {
+        stackdriver.end(res);
+      });
+    };
+  }
 
   // Create a Bunyan logger that streams to Cloud Logging
   // Logs will be written to: "projects/cavaccineinventory/logs/netlify"
   const logger = bunyan.createLogger({
     name: "netlify",
-    streams: [
-      // Log to the console at 'info' and above
-      { stream: process.stdout, level: "info" },
-      // And log to Cloud Logging, logging at 'info' and above
-      stackdriverStream,
-    ],
+    streams: streams,
   });
-
-  end_request = () => {
-    if (stackdriverStream.writableFinished) return;
-    return new Promise((res, rej) => {
-      stackdriver.end(res);
-    });
-  };
 
   request_logger = logger.child({
     request_id: event.headers["x-nf-request-id"],
