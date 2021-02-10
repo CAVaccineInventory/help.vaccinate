b7b6cecb01b2719ac8b7960e6999ba5eb1960cfe
Alex Vandiver <alex@chmrr.net>
2021-02-09 23:03:48 -0800

    Remove try/catch from public-test.
    This is now handled in the logger.

--- netlify/functions/public-test/public-test.js
+++ netlify/functions/public-test/public-test.js
@@ -4,18 +4,14 @@ const { loggedHandler } = require("../../lib/logger.js");
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = loggedHandler(async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || "World";
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    };
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() };
-  }
+  const subject = event.queryStringParameters.name || "World";
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: `Hello ${subject}` }),
+    // // more keys you can return:
+    // headers: { "headerName": "headerValue", ... },
+    // isBase64Encoded: true,
+  };
 });
 
 module.exports = { handler };
