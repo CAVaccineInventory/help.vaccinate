d05f0603ae6f417d84c179be2cd54c193a86ea66
Jesse Vincent <jesse@keyboard.io>
2021-02-12 19:38:58 -0800

    We're using optional chaining in these apps since we're only targeting current modern browsers

--- apps/.eslintrc.js
+++ apps/.eslintrc.js
@@ -3,4 +3,12 @@ module.exports = {
     "ecmaVersion": 12,
     "sourceType": "module",
   },
+  "overrides": [
+    {
+      "files": ["*.js"], 
+      "rules": {
+        "es/no-optional-chaining": "off"  // These internal tools are targeting current modern browsers. I want optional chaining
+      }
+    }
+   ]
 };
