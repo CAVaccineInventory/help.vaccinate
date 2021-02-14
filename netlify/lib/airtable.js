c24493994f3a114f456590cc15fc39daa799c7ee
Nick Martin <nim-github@nimlabs.org>
2021-02-14 12:34:57 -0800

    add new method to airtable to be able to use multiple airtable API keys at once.

--- netlify/lib/airtable.js
+++ netlify/lib/airtable.js
@@ -5,6 +5,25 @@ if (!process.env.AIRTABLE_API_KEY || !process.env.AIRTABLE_BASE) {
   // XXX error here?
 }
 
-const base = require("airtable").base(process.env.AIRTABLE_BASE);
+const Airtable = require('airtable');
 
+// uses the default connection which reads the API key from the environment.
+const base = Airtable.base(process.env.AIRTABLE_BASE);
 module.exports.base = base;
+
+
+// The AIRTABLE_READ_KEYS environment variable can be set to a comma
+// separated list of API keys. These keys may have read only access to
+// the database for extra safety.
+//
+// If this is not set or empty, it will default to using the same API
+// key as above, AIRTABLE_API_KEY
+const readKeys = (
+  process.env.AIRTABLE_READ_KEYS || process.env.AIRTABLE_API_KEY || '')
+      .split(',');
+const readClients = readKeys.map((k) => new Airtable({apiKey: k}));
+
+module.exports.getReadBase = () => {
+  const c = readClients[Math.floor(Math.random() * readClients.length)];
+  return c.base(process.env.AIRTABLE_BASE);
+};
