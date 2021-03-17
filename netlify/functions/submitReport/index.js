49f3688e5aee4c10c666b394bb64d537f75fee78
Alex Vandiver <alex@chmrr.net>
2021-03-17 10:54:59 -0700

    submitReport: Make dual-write to VIAL async with other work.
    Because it does nothing other than relay the POST body to VIAL, it
    can also start doing so earlier.
    
    Doing this re-POST to VIAL from within the `help.vaccinate` cloud
    function accomplishes little; in the future, this POST can and should
    be done directly by the client-side JS, rather than server-side.

--- netlify/functions/submitReport/index.js
+++ netlify/functions/submitReport/index.js
@@ -52,6 +52,27 @@ const handler = async (event, context, logger) => {
     };
   }
 
+  // Start the dual-write to VIAL; it is not authoritative, so we swallow its
+  // failures.  Because of that, it may also succeed even if we return 400 or
+  // 500 due to failures in the Airtable path.
+  awaits.push(
+    new Promise(async (resolve) => {
+      try {
+        await fetch("https://vial-staging.calltheshots.us/api/submitReport", {
+          method: "POST",
+          body: event.body,
+          headers: {
+            Authorization: `Bearer ${context.identityContext.token}`,
+          },
+        });
+      } catch (err) {
+        logger.error("failed to dual-write to VIAL", err);
+        // No re-raise; this is not authoritative.
+      }
+      resolve();
+    })
+  );
+
   // if locations is not a list, make it one. convenience.
   if (!Array.isArray(input.Location)) {
     input.Location = [input.Location];
@@ -85,21 +106,6 @@ const handler = async (event, context, logger) => {
   }
 
   const output = {};
-
-  // Start the dual-write to VIAL; it is not authoritative, so we swallow its
-  // failures.  Because of that, it may also succeed even if we return 400 or
-  // 500 due to failures in the Airtable path.
-  try {
-    await fetch("https://vial-staging.calltheshots.us/api/submitReport", {
-      method: "POST",
-      body: event.body,
-      headers: {
-        Authorization: `Bearer ${context.identityContext.token}`,
-      },
-    });
-  } catch (err) {
-    logger.error("failed to dual-write to VIAL", err);
-  }
 
   try {
     const createdReport = await base("Reports").create([{ fields: input }]);
