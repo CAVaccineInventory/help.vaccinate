f1f8b771595f8e45f45199dfcb14faaa6ca476ae
Alec Lee <alec.lee15@gmail.com>
2021-04-06 16:57:02 -0700

    run lint

--- apps/webpack.config.js
+++ apps/webpack.config.js
@@ -35,7 +35,5 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new webpack.EnvironmentPlugin({DEPLOY: 'development'})
-  ]
+  plugins: [new webpack.EnvironmentPlugin({ DEPLOY: "development" })],
 };
