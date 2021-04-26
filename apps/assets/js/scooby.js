b2b36d95dce77b90e15c27965d75f65a9c6b0d6a
Alec Lee <alec.lee15@gmail.com>
2021-04-26 16:25:41 -0700

    add sentry tracking to scooby + intentional break so we can ensure events go upstream

--- apps/assets/js/scooby.js
+++ apps/assets/js/scooby.js
@@ -1,5 +1,13 @@
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
+
 import { initScooby } from "./main.js";
 
 document.addEventListener("DOMContentLoaded", function () {
+  Sentry.init({
+    dsn: "https://f4f6dd9c4060438da4ae154183d9f7c6@o509416.ingest.sentry.io/5737071",
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
   initScooby();
 });
