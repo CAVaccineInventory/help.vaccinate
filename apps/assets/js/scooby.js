1868ab42737c6ad814d30db004c0a50fb5129541
Alec Lee <alec.lee15@gmail.com>
2021-04-26 18:02:12 -0700

    lower tracing sample rate to 20%

--- apps/assets/js/scooby.js
+++ apps/assets/js/scooby.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
   Sentry.init({
     dsn: "https://f4f6dd9c4060438da4ae154183d9f7c6@o509416.ingest.sentry.io/5737071",
     integrations: [new Integrations.BrowserTracing()],
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 0.2,
   });
   initScooby();
 });
