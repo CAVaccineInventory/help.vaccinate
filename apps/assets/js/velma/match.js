6901730c129b0e8130d91ba7c572f004b3c33f9f
Alec Lee <alec.lee15@gmail.com>
2021-05-13 14:39:49 -0700

    simplify velma logic files

--- apps/assets/js/velma/match.js
+++ apps/assets/js/velma/match.js
@@ -7,113 +7,6 @@ import matchActionsTemplate from "../templates/velma/matchActions.handlebars";
 import matchKeybindsTemplate from "../templates/velma/matchKeybinds.handlebars";
 
 export const matchLogic = () => {
-  const getData = async (id, onError) => {
-    const user = await getUser();
-    const response = await fetchJsonFromEndpoint(`/searchSourceLocations?${createSearchQueryParams(id)}`, "GET");
-    if (response.error) {
-      showErrorModal(
-        "Error fetching source location",
-        "We ran into an error trying to fetch you a source location to match. Please show this error message to your captain or lead on Slack." +
-            " They may also need to know that you are logged in as " +
-            user?.email +
-            ".",
-        response
-      );
-      onError();
-      return;
-    } else if (response.results && !response.results.length) {
-      // no results
-      showErrorModal(
-        "No locations to match",
-        "It looks like we've matched every single source location for the provided query parameters!",
-        createSearchQueryParams(id)
-      );
-      onError();
-      return;
-    }
-
-    const sourceLocation = response.results[0];
-    const currentLocationDebugJson = JSON.stringify(sourceLocation, null, 2);
-
-    // massage sourceLocation into a standard location object
-    const currentLocation = {};
-    currentLocation.id = sourceLocation?.import_json?.id;
-    currentLocation.name = sourceLocation.name;
-    currentLocation.latitude = Math.round(sourceLocation.latitude * 10000) / 10000;
-    currentLocation.longitude = Math.round(sourceLocation.longitude * 10000) / 10000;
-
-    const websites = sourceLocation?.import_json?.contact?.filter((method) => !!method.website);
-    currentLocation.website =
-        websites?.find((method) => method.contact_type === "general")?.website || websites?.[0]?.website;
-    currentLocation.phone_number = sourceLocation?.import_json?.contact?.find((method) => !!method.phone)?.phone;
-    currentLocation.full_address = `${sourceLocation.import_json.address.street1}, ${sourceLocation.import_json.address.city}, ${sourceLocation.import_json.address.state} ${sourceLocation.import_json.address.zip}`;
-
-    const candidates = await createCandidates(sourceLocation, null, (error) => {
-      showErrorModal(
-        "Error fetching locations to match against",
-        "We ran into an error trying to fetch you locations to match against. Please show this error message to your captain or lead on Slack." +
-            " They may also need to know that you are logged in as " +
-            user?.email +
-            ".",
-        error
-      );
-      onError();
-      return;
-    });
-
-    return {
-      currentLocation,
-      currentLocationDebugJson,
-      candidates,
-    };
-  };
-
-  const initActions = (currentLocation, candidate, actions) => {
-    fillTemplateIntoDom(matchActionsTemplate, "#actionsContainer", {
-      candidate,
-    });
-
-    bindClick(".js-skip", actions.skipLocation);
-    bindClick(".js-tryagain", actions.restart);
-    bindClick(".js-close", actions.dismissItem);
-    bindClick(".js-match", () => !!candidate && matchLocation(currentLocation?.id, candidate.id, actions.completeLocation));
-    bindClick(".js-create", () => createLocation(currentLocation?.id, actions.completeLocation));
-  };
-
-  const handleKeybind = (key, currentLocation, candidate, actions) => {
-    switch (key) {
-      case "1":
-      case "m":
-        if (candidate?.id) {
-          document.querySelector(".js-match")?.classList?.add("active");
-          matchLocation(currentLocation?.id, candidate.id, actions.completeLocation);
-        }
-        break;
-      case "2":
-      case "d":
-        if (candidate?.id) {
-          actions.dismissItem();
-        } else {
-          actions.restart();
-        }
-        break;
-      case "3":
-      case "c":
-        document.querySelector(".js-create")?.classList?.add("active");
-        createLocation(currentLocation?.id, actions.completeLocation);
-        break;
-      case "4":
-      case "s":
-        document.querySelector(".js-skip")?.classList?.add("active");
-        actions.skipLocation();
-        break;
-      case "5":
-      case "u":
-        actions.undoPreviousLocation();
-        break;
-    }
-  };
-
   return {
     getData,
     initActions,
@@ -123,6 +16,114 @@ export const matchLogic = () => {
     keybindTemplate: matchKeybindsTemplate,
   };
 };
+
+const getData = async (id, onError) => {
+  const user = await getUser();
+  const response = await fetchJsonFromEndpoint(`/searchSourceLocations?${createSearchQueryParams(id)}`, "GET");
+  if (response.error) {
+    showErrorModal(
+      "Error fetching source location",
+      "We ran into an error trying to fetch you a source location to match. Please show this error message to your captain or lead on Slack." +
+            " They may also need to know that you are logged in as " +
+            user?.email +
+            ".",
+      response
+    );
+    onError();
+    return;
+  } else if (response.results && !response.results.length) {
+    // no results
+    showErrorModal(
+      "No locations to match",
+      "It looks like we've matched every single source location for the provided query parameters!",
+      createSearchQueryParams(id)
+    );
+    onError();
+    return;
+  }
+
+  const sourceLocation = response.results[0];
+  const currentLocationDebugJson = JSON.stringify(sourceLocation, null, 2);
+
+  // massage sourceLocation into a standard location object
+  const currentLocation = {};
+  currentLocation.id = sourceLocation?.import_json?.id;
+  currentLocation.name = sourceLocation.name;
+  currentLocation.latitude = Math.round(sourceLocation.latitude * 10000) / 10000;
+  currentLocation.longitude = Math.round(sourceLocation.longitude * 10000) / 10000;
+
+  const websites = sourceLocation?.import_json?.contact?.filter((method) => !!method.website);
+  currentLocation.website =
+        websites?.find((method) => method.contact_type === "general")?.website || websites?.[0]?.website;
+  currentLocation.phone_number = sourceLocation?.import_json?.contact?.find((method) => !!method.phone)?.phone;
+  currentLocation.full_address = `${sourceLocation.import_json.address.street1}, ${sourceLocation.import_json.address.city}, ${sourceLocation.import_json.address.state} ${sourceLocation.import_json.address.zip}`;
+
+  const candidates = await createCandidates(sourceLocation, null, (error) => {
+    showErrorModal(
+      "Error fetching locations to match against",
+      "We ran into an error trying to fetch you locations to match against. Please show this error message to your captain or lead on Slack." +
+            " They may also need to know that you are logged in as " +
+            user?.email +
+            ".",
+      error
+    );
+    onError();
+    return;
+  });
+
+  return {
+    currentLocation,
+    currentLocationDebugJson,
+    candidates,
+  };
+};
+
+const initActions = (currentLocation, candidate, actions) => {
+  fillTemplateIntoDom(matchActionsTemplate, "#actionsContainer", {
+    candidate,
+  });
+
+  bindClick(".js-skip", actions.skipLocation);
+  bindClick(".js-tryagain", actions.restart);
+  bindClick(".js-close", actions.dismissItem);
+  bindClick(".js-match", () => !!candidate && matchLocation(currentLocation?.id, candidate.id, actions.completeLocation));
+  bindClick(".js-create", () => createLocation(currentLocation?.id, actions.completeLocation));
+};
+
+const handleKeybind = (key, currentLocation, candidate, actions) => {
+  switch (key) {
+    case "1":
+    case "m":
+      if (candidate?.id) {
+        document.querySelector(".js-match")?.classList?.add("active");
+        matchLocation(currentLocation?.id, candidate.id, actions.completeLocation);
+      }
+      break;
+    case "2":
+    case "d":
+      if (candidate?.id) {
+        actions.dismissItem();
+      } else {
+        actions.restart();
+      }
+      break;
+    case "3":
+    case "c":
+      document.querySelector(".js-create")?.classList?.add("active");
+      createLocation(currentLocation?.id, actions.completeLocation);
+      break;
+    case "4":
+    case "s":
+      document.querySelector(".js-skip")?.classList?.add("active");
+      actions.skipLocation();
+      break;
+    case "5":
+    case "u":
+      actions.undoPreviousLocation();
+      break;
+  }
+};
+
 
 const matchLocation = async (currentLocationId, candidateId, completeLocation) => {
   showLoadingScreen();
--- apps/assets/js/velma/merge.js
+++ apps/assets/js/velma/merge.js
@@ -7,113 +7,6 @@ import mergeActionsTemplate from "../templates/velma/mergeActions.handlebars";
 import mergeKeybindsTemplate from "../templates/velma/mergeKeybinds.handlebars";
 
 export const mergeLogic = () => {
-  const getData = async (id, onError) => {
-    if (id) {
-      console.error("merge logic doesn't support forced IDs");
-      return;
-    }
-    const user = await getUser();
-    const response = await fetchJsonFromEndpoint("/requestTask", "POST", JSON.stringify({
-      task_type: "Potential duplicate",
-    }));
-    if (response.error) {
-      showErrorModal(
-        "Error fetching location to merge",
-        "We ran into an error trying to fetch you a location to merge. Please show this error message to your captain or lead on Slack." +
-            " They may also need to know that you are logged in as " +
-            user?.email +
-            ".",
-        response
-      );
-      onError();
-      return;
-    }
-    if (!response.task) {
-      showErrorModal(
-        "No locations left to merge",
-        "We could not find a location that needed to be merged:",
-        response
-      );
-      onError();
-      return;
-    }
-    const currentLocation = response.task.location;
-    const currentLocationDebugJson = JSON.stringify(currentLocation, null, 2);
-    // add taskId to current location to later resolve
-    currentLocation.task_id = response.task.id;
-
-    const candidates = await createCandidates(currentLocation, response.task.other_location, (error) => {
-      showErrorModal(
-        "Error fetching locations to merge against",
-        "We ran into an error trying to fetch you locations to merge against. Please show this error message to your captain or lead on Slack." +
-            " They may also need to know that you are logged in as " +
-            user?.email +
-            ".",
-        error
-      );
-      onError();
-      return;
-    });
-
-    return {
-      currentLocation,
-      currentLocationDebugJson,
-      candidates,
-    };
-  };
-
-  const initActions = (currentLocation, candidate, actions) => {
-    fillTemplateIntoDom(mergeActionsTemplate, "#actionsContainer", {
-      candidate,
-    });
-
-    bindClick(".js-skip", actions.skipLocation);
-    bindClick(".js-tryagain", actions.restart);
-    bindClick(".js-close", actions.dismissItem);
-    bindClick(".js-current-wins", () => mergeLocations(currentLocation.id, candidate.id, currentLocation.task_id, actions.completeLocation));
-    bindClick(".js-candidate-wins", () => mergeLocations(candidate.id, currentLocation.id, currentLocation.task_id, actions.completeLocation));
-    bindClick(".js-no-merges", () => resolveTask(currentLocation.task_id, actions.completeLocation));
-  };
-
-  const handleKeybind = (key, currentLocation, candidate, actions) => {
-    switch (key) {
-      case "1":
-      case "r":
-        if (currentLocation.id && candidate.id && currentLocation.task_id) {
-          document.querySelector(".js-current-wins")?.classList?.add("active");
-          mergeLocations(currentLocation.id, candidate.id, currentLocation.task_id, actions.completeLocation);
-        }
-        break;
-      case "2":
-      case "b":
-        if (currentLocation.id && candidate.id && currentLocation.task_id) {
-          document.querySelector(".js-candidate-wins")?.classList?.add("active");
-          mergeLocations(candidate.id, currentLocation.id, currentLocation.task_id, actions.completeLocation);
-        }
-        break;
-      case "3":
-      case "d":
-        if (candidate?.id) {
-          actions.dismissItem();
-        } else {
-          actions.restart();
-        }
-        break;
-      case "4":
-      case "s":
-        document.querySelector(".js-skip")?.classList?.add("active");
-        actions.skipLocation();
-        break;
-      case "5":
-      case "n":
-        if (currentLocation.task_id) {
-          document.querySelector(".js-no-merges")?.classList?.add("active");
-          resolveTask(currentLocation.task_id, actions.completeLocation);
-        }
-        break;
-    }
-  };
-
   return {
     getData,
     initActions,
@@ -123,6 +16,114 @@ export const mergeLogic = () => {
     keybindTemplate: mergeKeybindsTemplate,
   };
 };
+
+const getData = async (id, onError) => {
+  if (id) {
+    console.error("merge logic doesn't support forced IDs");
+    return;
+  }
+  const user = await getUser();
+  const response = await fetchJsonFromEndpoint("/requestTask", "POST", JSON.stringify({
+    task_type: "Potential duplicate",
+  }));
+  if (response.error) {
+    showErrorModal(
+      "Error fetching location to merge",
+      "We ran into an error trying to fetch you a location to merge. Please show this error message to your captain or lead on Slack." +
+            " They may also need to know that you are logged in as " +
+            user?.email +
+            ".",
+      response
+    );
+    onError();
+    return;
+  }
+  if (!response.task) {
+    showErrorModal(
+      "No locations left to merge",
+      "We could not find a location that needed to be merged:",
+      response
+    );
+    onError();
+    return;
+  }
+  const currentLocation = response.task.location;
+  const currentLocationDebugJson = JSON.stringify(currentLocation, null, 2);
+  // add taskId to current location to later resolve
+  currentLocation.task_id = response.task.id;
+
+  const candidates = await createCandidates(currentLocation, response.task.other_location, (error) => {
+    showErrorModal(
+      "Error fetching locations to merge against",
+      "We ran into an error trying to fetch you locations to merge against. Please show this error message to your captain or lead on Slack." +
+            " They may also need to know that you are logged in as " +
+            user?.email +
+            ".",
+      error
+    );
+    onError();
+    return;
+  });
+
+  return {
+    currentLocation,
+    currentLocationDebugJson,
+    candidates,
+  };
+};
+
+const initActions = (currentLocation, candidate, actions) => {
+  fillTemplateIntoDom(mergeActionsTemplate, "#actionsContainer", {
+    candidate,
+  });
+
+  bindClick(".js-skip", actions.skipLocation);
+  bindClick(".js-tryagain", actions.restart);
+  bindClick(".js-close", actions.dismissItem);
+  bindClick(".js-current-wins", () => mergeLocations(currentLocation.id, candidate.id, currentLocation.task_id, actions.completeLocation));
+  bindClick(".js-candidate-wins", () => mergeLocations(candidate.id, currentLocation.id, currentLocation.task_id, actions.completeLocation));
+  bindClick(".js-no-merges", () => resolveTask(currentLocation.task_id, actions.completeLocation));
+};
+
+const handleKeybind = (key, currentLocation, candidate, actions) => {
+  switch (key) {
+    case "1":
+    case "r":
+      if (currentLocation.id && candidate.id && currentLocation.task_id) {
+        document.querySelector(".js-current-wins")?.classList?.add("active");
+        mergeLocations(currentLocation.id, candidate.id, currentLocation.task_id, actions.completeLocation);
+      }
+      break;
+    case "2":
+    case "b":
+      if (currentLocation.id && candidate.id && currentLocation.task_id) {
+        document.querySelector(".js-candidate-wins")?.classList?.add("active");
+        mergeLocations(candidate.id, currentLocation.id, currentLocation.task_id, actions.completeLocation);
+      }
+      break;
+    case "3":
+    case "d":
+      if (candidate?.id) {
+        actions.dismissItem();
+      } else {
+        actions.restart();
+      }
+      break;
+    case "4":
+    case "s":
+      document.querySelector(".js-skip")?.classList?.add("active");
+      actions.skipLocation();
+      break;
+    case "5":
+    case "n":
+      if (currentLocation.task_id) {
+        document.querySelector(".js-no-merges")?.classList?.add("active");
+        resolveTask(currentLocation.task_id, actions.completeLocation);
+      }
+      break;
+  }
+};
+
 
 const mergeLocations = async (winner, loser, taskId, completeLocation) => {
   showLoadingScreen();
